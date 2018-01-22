955068eb12278e593781df790e6a3246ac239763
SteveDengZishi <zd475@nyu.edu>
2018-01-22 16:49:17 -0500

    file.go

--- file.go
+++ file.go
@@ -3,8 +3,42 @@ package main
 import(
 	"fmt"
 	"os"
+	"io"
 )
 
+func checkErr(err error){
+	if err != nil{
+		//interupt the program
+		panic(err)
+	}
+}
+
 func main(){
-	
+	//Reading from file without encapsulation
+	infile, err := os.Open("data.csv")
+
+	if err == nil{
+		fmt.Println(infile)
+	} else {
+		fmt.Println(err)
+	}
+
+	//Writing to file
+	outfile, err := os.Create("data.csv")
+	checkErr(err)
+	defer outfile.Close()
+
+	ln, err := io.WriteString(outfile, "New String")
+	checkErr(err)
+	fmt.Printf("All done with file of %v characters\n", ln)
+
+	//Reading from file
+	infile, err2 := os.Open("data.csv")
+
+	if err2 == nil{
+		fmt.Println(infile)
+	} else {
+		fmt.Println(err)
+	}
+
 }
